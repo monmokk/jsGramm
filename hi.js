2fcd576f807ca1d2156dab97bfb7f3a83f7cc48b
mjk <myeongjikang910814@gmail.com>
2022-05-06 02:04:19 +0900

    Initial commit

---
+++ hi.js
@@ -0,0 +1,6 @@
+console.log('hi')
+
+const shoesPrice = 200000
+console.log(`이 신발의 가격은 ${shoesPrice}원입니다`)
+// console.log('이 신발의 가격은 ' + shoesPrice + '원입니다') 와 동일
+// + 를 활용한 문자열 붙이기보다 간결하게 표현할 수 있다는 게 보이시나요?
